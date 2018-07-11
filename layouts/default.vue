9cbe56385bf9b00d4b4865f88d388f197498aa5e
zzlhr <2388399752@qq.com>
2018-07-11 18:32:16 +0800

    init commit

---
+++ layouts/default.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="text-center blog-title">
+      <b-img rounded="circle" src="https://avatars0.githubusercontent.com/u/25954503?s=460&v=4" width="75" height="75" alt="img" class="m-1" ></b-img>
+      <h2>刘浩然的博客</h2>
+      <div></div>
+    </div>
+    <div class="blog-nav">
+      <b-container>
+        <b-nav>
+          <b-nav-item active>首页</b-nav-item>
+          <b-nav-item>文章</b-nav-item>
+          <b-nav-item>关于</b-nav-item>
+        </b-nav>
+      </b-container>
+    </div>
+    <nuxt/>
+    <div class="blog-footer text-center">
+      <p class="">© 2018 刘浩然的博客.</p>
+    </div>
+  </div>
+</template>
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Acme|Asap|Cabin+Sketch|Kalam|Poiret+One');
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif,
+  'Cabin Sketch', cursive,'Kalam', cursive, 'Poiret One', cursive,'Acme', sans-serif,'Asap', sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+  color: #47494e;
+}
+
+*, *:before, *:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,94 @@
+<template>
+  <b-container>
+    <div class="row">
+      <div class="col-lg-8 blog-content">
+        <div class="blog-list">
+          <div class="blog-item" v-for="article in articles" :key="article.articleId">
+            <div class="blog-content">
+              <h5 class="blog-content-title"><a href="article/123/">{{article.articleTitle}}</a></h5>
+              <!--<small>由 lhr 发表于 2018-7-7 13:20:03 更新与 2018-7-7 13:20:03    点击数: 20    评论数: 0    点赞数: 0</small>-->
+              <p class="blog-content-describe">{{article.articleDescribe}}</p>
+              <div class="blog-content-footer">
+                <span href="about"><i class="iconfont icon-tagfill"></i>&nbsp;<a href="Mac">Mac</a></span>
+                <span><i class="iconfont icon-myfill"></i>&nbsp;<a href="about/lhr/">{{article.articleAuthor.name}}</a></span>
+                <span href="about"><i class="iconfont icon-msnui-time-bold"></i>&nbsp;{{article.createTime}}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 blog-right">
+        <div class="blog-list blog-right-list">
+          <div class="blog-item">
+            <label class="blog-item-title">站长信息</label>
+            <div class="blog-item-body">
+              <p class="h6">刘浩然</p>
+              <p>
+                一个资深技术宅的站点
+              </p>
+              <div class="blog-link">
+                <a href="mailto:lhr@lhrsite.com"><i class="iconfont icon-mail"></i></a>
+                <a href="https://github.com/zzlhr/"><i class="iconfont icon-github"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </b-container>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  },
+  data(){
+    return {
+      articles:[
+        {
+          articleId:1,
+          articleTitle: 'IDEA 正则批量替换',
+          articleDescribe:'场景给实体类批量添加默认值，属性多加上类型多手动换起来太麻烦。',
+          articleAuthor:{
+            id:1,
+            name: 'lhr'
+          },
+          createTime:'2018-06-13 11:43:42'
+        },
+        {
+          articleId:2,
+          articleTitle: 'apache ab 压力测试工具-1',
+          articleDescribe:'记录apache ab学习使用过程.',
+          articleAuthor:{
+            id:1,
+            name: 'lhr'
+          },
+          createTime:'2018-05-17 14:51:21'
+        },
+        {
+          articleId:3,
+          articleTitle: 'vue-resource出现Cannot read property \'post\' of undefined得解决方案',
+          articleDescribe:'使用webpack打包按照官方文档引用出现\'post\' of undefined的解决方案。',
+          articleAuthor:{
+            id:1,
+            name: 'lhr'
+          },
+          createTime:'2018-05-04 09:06:58'
+        }
+
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.b-container{
+  display: flex;
+  flex-wrap: nowrap;
+  flex-direction: row;
+}
+</style>
